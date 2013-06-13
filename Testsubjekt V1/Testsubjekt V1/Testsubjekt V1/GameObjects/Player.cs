eb4ddb0f57b9ce7b047c8d60d2b1939988112387
PitchBlecc <florianheinrich@t-online.de>
2013-06-13 21:01:50 +0200

    Tests an verbesserter Collision auskommentiert, nicht komplett funktionstüchtig

--- Testsubjekt V1/Testsubjekt V1/Testsubjekt V1/GameObjects/Player.cs
+++ Testsubjekt V1/Testsubjekt V1/Testsubjekt V1/GameObjects/Player.cs
@@ -69,26 +69,89 @@ namespace TestsubjektV1
             float forward = (Keyboard.GetState().IsKeyDown(Keys.W) ? 1.0f : 0.0f) + (Keyboard.GetState().IsKeyDown(Keys.Up) ? 1.0f : 0.0f) -
                             (Keyboard.GetState().IsKeyDown(Keys.S) ? 1.0f : 0.0f) - (Keyboard.GetState().IsKeyDown(Keys.Down) ? 1.0f : 0.0f);
 
-
-            this.position += forward * speed * front;
             Vector3 sideVec = Vector3.Cross(front, new Vector3(0,1,0));
             float side = (Keyboard.GetState().IsKeyDown(Keys.D) ? 1.0f : 0.0f) + (Keyboard.GetState().IsKeyDown(Keys.Right) ? 1.0f : 0.0f) -
                          (Keyboard.GetState().IsKeyDown(Keys.A) ? 1.0f : 0.0f) - (Keyboard.GetState().IsKeyDown(Keys.Left) ? 1.0f : 0.0f);
 
+            this.position += forward * speed * front;
             this.position += side * speed * sideVec;
 
             int x1 = (int)Math.Round(-1 * this.position.X + Constants.MAP_SIZE-1) / 2;
             int x2 = (int)Math.Round(-1 * this.position.X + Constants.MAP_SIZE) / 2;
             int z1 = (int)Math.Round(-1 * this.position.Z + Constants.MAP_SIZE-1) / 2;
             int z2 = (int)Math.Round(-1 * this.position.Z + Constants.MAP_SIZE) / 2;
-            if ((world.MapData[x1][z1] == '1') || (world.MapData[x2][z2] == '1') || (world.MapData[x2][z1] == '1') || (world.MapData[x1][z2] == '1'))
-            {
-                this.position = this.model.Position;
-                this.model.Position = this.position;
+
+            /*if (world.MapData[x2][z2] == '1')
+            { 
+                if(world.MapData[x1][z2] == '0')
+                {
+                    front = new Vector3(0, 0, front.Z);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else if(world.MapData[x2][z1] == '0')
+                {
+                    front = new Vector3(front.X, 0, 0);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else this.position = this.model.Position;
             }
-            else model.Position = this.position;
+            else if (world.MapData[x1][z2] == '1')
+            {
+                if (world.MapData[x2][z2] == '0')
+                {
+                    front = new Vector3(0, 0, front.Z);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else if (world.MapData[x1][z1] == '0')
+                {
+                    front = new Vector3(front.X, 0, 0);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else this.position = this.model.Position;
+            }
+            else if (world.MapData[x1][z1] == '1')
+            {
+                if (world.MapData[x2][z1] == '0')
+                {
+                    front = new Vector3(0, 0, front.Z);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else if (world.MapData[x1][z2] == '0')
+                {
+                    front = new Vector3(front.X, 0, 0);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else this.position = this.model.Position;
+            }
+            else if (world.MapData[x2][z1] == '1')
+            {
+                if (world.MapData[x1][z1] == '0')
+                {
+                    front = new Vector3(0, 0, front.Z);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else if (world.MapData[x2][z1] == '0')
+                {
+                    front = new Vector3(front.X, 0, 0);
+                    this.model.Position += forward * speed * front;
+                    this.position = this.model.Position;
+                }
+                else this.position = this.model.Position;
+            }*/
+
+            if ((world.MapData[x1][z1] == '1') || (world.MapData[x1][z2] == '1') || (world.MapData[x2][z1] == '1') || (world.MapData[x2][z2] == '1'))
+                this.position = this.model.Position;
+            else this.model.Position = this.position;
 
             weapon.update(bullets, position, direction);
+
             return true;
         }
 
